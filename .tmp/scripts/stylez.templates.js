f70ea28210d5429caf03d41fbcfd9460188859a6
Stefan Bauer <stefan.bauer@n8d.at>
2020-07-30 23:54:34 +0200

    Added elevation styles

---
+++ .tmp/scripts/stylez.templates.js
@@ -0,0 +1,8 @@
+Handlebars.registerPartial("01-styles-elevation",Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"util-flex\">\n\n    <div class=\"utilbox depth-4\">\n        depth 4\n    </div>\n    <div class=\"utilbox depth-8\">\n        depth 8\n    </div>\n    <div class=\"utilbox depth-16\">\n        depth 16\n    </div>\n    <div class=\"utilbox depth-32\">\n        depth 32\n    </div>\n    <div class=\"utilbox depth-48\">\n        depth 48\n    </div>\n    <div class=\"utilbox depth-64\">\n        depth 64\n    </div>\n\n</div>";
+},"useData":true}));
+this["stylez"] = this["stylez"] || {};
+this["stylez"]["templates"] = this["stylez"]["templates"] || {};
+this["stylez"]["templates"]["01-styles-elevation"] = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<div class=\"util-flex\">\n\n    <div class=\"utilbox depth-4\">\n        depth 4\n    </div>\n    <div class=\"utilbox depth-8\">\n        depth 8\n    </div>\n    <div class=\"utilbox depth-16\">\n        depth 16\n    </div>\n    <div class=\"utilbox depth-32\">\n        depth 32\n    </div>\n    <div class=\"utilbox depth-48\">\n        depth 48\n    </div>\n    <div class=\"utilbox depth-64\">\n        depth 64\n    </div>\n\n</div>";
+},"useData":true});
