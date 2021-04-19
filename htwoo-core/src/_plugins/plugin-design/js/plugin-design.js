dac79c4b52a17f5d169f5e2e7d2713ec7ac9b0bc
Stefan Bauer <office@n8d.at>
2021-04-19 12:32:38 +0200

    Local storage handling and design switches

--- htwoo-core/src/_plugins/plugin-design/js/plugin-design.js
+++ htwoo-core/src/_plugins/plugin-design/js/plugin-design.js
@@ -1,10 +1,26 @@
-/* global PluginUIExtension */
+'use strict';
+/* global PluginUIExtension, pluginDesign */
+
 var PluginUIExtension = {
 
   /**
    * The function defined as the onready callback within the plugin configuration.
    */
   init: function () {
+
+    console.log(window);
+    console.log(window.parent);
+    console.log(window.localStorage);
+
+    window.dispatchEvent(new Event('storage'));
+
+    window.addEventListener('storage', () => {
+      // When local storage changes, dump the list to
+      // the console.
+      console.log(JSON.parse(window.localStorage.getItem('app')));
+    });
+
+
 
     let logoImg = document.querySelector(".pl-c-logo__img");
     if (logoImg) {
@@ -22,4 +38,5 @@ var PluginUIExtension = {
     }
 
   }
+
 };
