6962b880ac11824c1a9d52d833bb6ac422275f18
Julie Turner <julie@julieturner.net>
2023-01-16 21:37:13 +0000

    Fixed overflow observer bug
    Fixed typo in htwoo-core pivot.js

--- htwoo-react/src/components/common/OverflowObserver.ts
+++ htwoo-react/src/components/common/OverflowObserver.ts
@@ -13,7 +13,7 @@ export interface IOverflowResizer {
 }
 
 export class OverflowResizer implements IOverflowResizer {
-  private LOG_SOURCE: string = "🔶OverflowResizer";
+  private LOG_SOURCE: string = "💦OverflowResizer";
   private _instanceId: string;
   private _resizeObserver: ResizeObserver;
   private _resizeContainer: HTMLElement;
@@ -50,19 +50,21 @@ export class OverflowResizer implements IOverflowResizer {
 
   private _initOverflowElements = (children) => {
     try {
-      this._overflowItems = [];
-      let overallWidth = 0;
-      for (let i = 0; i < children.length; i++) {
-        overallWidth += children[i].clientWidth;
-        if (!children[i].classList.contains("hoo-buttonicon-overflow")) {
-          let currentItem = {
-            child: children[i],
-            ref: `ref-${this._instanceId}-${i}`,
-            width: children[i].clientWidth,
-            overallWidth: overallWidth
-          };
-          children[i].dataset.ref = currentItem.ref;
-          this._overflowItems.push(currentItem);
+      if (this._overflowItems.length < children.length) {
+        this._overflowItems = [];
+        let overallWidth = 0;
+        for (let i = 0; i < children.length; i++) {
+          overallWidth += children[i].clientWidth;
+          if (!children[i].classList.contains("hoo-buttonicon-overflow")) {
+            let currentItem = {
+              child: children[i],
+              ref: `ref-${this._instanceId}-${i}`,
+              width: children[i].clientWidth,
+              overallWidth: overallWidth
+            };
+            children[i].dataset.ref = currentItem.ref;
+            this._overflowItems.push(currentItem);
+          }
         }
       }
     } catch (err) {
@@ -81,9 +83,10 @@ export class OverflowResizer implements IOverflowResizer {
 
   private _getOverflowItems = () => {
     try {
-      const overflowButton = this._resizeContainer.querySelector('.hoo-buttonicon-overflow');
+      //const overflowButton = this._resizeContainer.querySelector('.hoo-buttonicon-overflow');
       const defaultOffset = 40;
       const targetWidth = this._resizeContainer.parentElement.clientWidth;
+
       let curOverFlowItems = this._overflowItems.filter(item => {
         return item.overallWidth > targetWidth - defaultOffset;
       });
@@ -97,10 +100,9 @@ export class OverflowResizer implements IOverflowResizer {
       // Moves the Element into a new list item with all Events attached
       if (overflowControl && overflowControl.children.length < curOverFlowItems.length) {
         for (let i = 0; i < curOverFlowItems.length; i++) {
-          if (this._resizeContainer.querySelector("[data-ref=" + curOverFlowItems[i].ref + "]") !== null) {
-            let listItem = document.createElement('li');
-            const overflow = this._resizeContainer.querySelector("[data-ref=" + curOverFlowItems[i].ref + "]");
-            overflow.setAttribute("aria-hidden", "true");
+          if (this._resizeContainer.querySelector(`[data-ref=${curOverFlowItems[i].ref}]`) !== null) {
+            const listItem = document.createElement('li');
+            const overflow = this._resizeContainer.querySelector(`[data-ref=${curOverFlowItems[i].ref}]`);
             listItem.appendChild(overflow);
             overflowControl.appendChild(listItem);
           }
@@ -113,9 +115,8 @@ export class OverflowResizer implements IOverflowResizer {
       // Move elements back from overflow menu
       if (overflowControl && overflowControl.children.length > curOverFlowItems.length) {
         for (let i = 0; i < curItems.length; i++) {
-          if (overflowControl.querySelector("[data-ref=" + curItems[i].ref + "]") !== null) {
-            let overflowElement = overflowControl.querySelector("[data-ref=" + curItems[i].ref + "]");
-            overflowElement.removeAttribute("aria-hidden");
+          if (overflowControl.querySelector(`[data-ref=${curItems[i].ref}]`) !== null) {
+            let overflowElement = overflowControl.querySelector(`[data-ref=${curItems[i].ref}]`);
             this._resizeContainer.appendChild(overflowElement);
           }
         }
