93aab908954d310d0101ce90a5621998eac7854d
Stefan Bauer <stefan.bauer@n8d.at>
2024-06-13 19:19:06 +0200

    fix(js): hoo-nav accessiblity behaviour update

--- htwoo-core/src/js/nav.js
+++ htwoo-core/src/js/nav.js
@@ -1,21 +1,58 @@
+"use strict";
 /** MENU ITEM */
 
 const handleMenuItems = (event) => {
 
     let curNavItem = event.target;
     let curNavMenu = curNavItem.closest('.hoo-navitem');
-
-    // console.log(curNavMenu);
-    // console.log(curNavMenu.getAttribute('aria-expanded'));
-    // console.log(typeof curNavMenu.getAttribute('aria-expanded'));
+    let curNavMenuAll = curNavMenu.parentNode.querySelectorAll('.hoo-navitem');
 
     if (curNavMenu.getAttribute('aria-expanded') === 'false') {
 
         curNavMenu.setAttribute('aria-expanded', true);
+        curNavMenu.setAttribute('aria-selected', true);
+        curNavMenu.setAttribute('aria-current', true);
 
     } else {
 
         curNavMenu.setAttribute('aria-expanded', false);
+        curNavMenu.setAttribute('aria-selected', false);
+        curNavMenu.setAttribute('aria-current', false);
+
+    }
+
+}
+
+const resetAria = (items) => {
+    console.debug('All Items', items);
+    items.forEach((item) => {
+        item.removeAttribute('aria-current');
+    });
+}
+
+const handleNavItemClick = (event) => {
+
+    let curClickItem = event.target;
+
+    if (curClickItem.classList.contains('hoo-navitem-link')) {
+
+        let curNavItem = curClickItem.closest('.hoo-navitem');
+
+        let curNavMenuItems = curClickItem.closest('.hoo-nav').querySelectorAll('* .hoo-navitem');
+        
+        resetAria(curNavMenuItems);
+
+        curNavItem.setAttribute('aria-current', true);
+
+    } else if (curClickItem.classList.contains('hoo-icon')) {
+
+        let curNavItem = curClickItem.closest('.hoo-navitem');
+        
+        if (curNavItem.getAttribute('aria-expanded') === 'true') {
+            curNavItem.setAttribute('aria-expanded', false);
+        } else {
+            curNavItem.setAttribute('aria-expanded', true);
+        }
 
     }
 
@@ -23,13 +60,26 @@ const handleMenuItems = (event) => {
 
 export const initMenu = () => {
 
-    let menuItems = document.querySelectorAll('.hoo-navitem[aria-expanded]');
+    let navMenus = document.querySelectorAll('.hoo-nav');
 
-    menuItems.forEach(item => {
+    navMenus.forEach(nav => {
 
-        item.addEventListener('click', handleMenuItems);
+        let navItems = nav.querySelectorAll('.hoo-navitem');
+        navItems.forEach(navItem => {
+
+            navItem.addEventListener('click', handleNavItemClick, {});
+
+        })
 
     })
+
+    // let menuItems = document.querySelectorAll('.hoo-navitem[aria-expanded]');
+
+    // menuItems.forEach(item => {
+
+    //     item.addEventListener('click', handleMenuItems);
+
+    // })
 
 
 }
