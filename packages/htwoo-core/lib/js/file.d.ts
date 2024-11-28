d2c1016024753bb5065ab37f90e8a0f81857f03c
Stefan Bauer <stefan.bauer@n8d.at>
2024-11-28 20:07:01 +0100

    chore: update package content and documentation

---
+++ packages/htwoo-core/lib/js/file.d.ts
@@ -0,0 +1,13 @@
+export class FileUploadHandler {
+    constructor(htmlElement: any);
+    _files: any[];
+    _fileLable: any;
+    _fileControl: any;
+    _originalFileControl: any;
+    _infileOutput: any;
+    dragOver: (event: any) => void;
+    dragLeave: (event: any) => void;
+    dragDrop: (event: any) => void;
+    fileChangedEvent: (event: any) => void;
+    _fileLabel: any;
+}
